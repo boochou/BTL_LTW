ad4180eed27175accae88efa286314d5adafb159
MangCau <namanhcute2809@gmail.com>
2024-05-05 00:13:25 +0700

    add image for blog

--- controller/seller/addBlog.php
+++ controller/seller/addBlog.php
@@ -4,14 +4,27 @@ include_once("../../model/connectdb.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve the data from the POST request
     $data = json_decode(file_get_contents("php://input"));
-    $content = mysqli_real_escape_string($mysqli, $data->content); // Escape input
     $title = mysqli_real_escape_string($mysqli, $data->title); // Escape input
+    $content = mysqli_real_escape_string($mysqli, $data->content); // Escape input
+    $images = $data->images;
+
+    $uploadDirectory = '../../public/BlogImage/';
+    $imagePaths = array();
+
+    // Save base64 images to files
+    foreach ($images as $base64Image) {
+        $imageName = uniqid() . '.png'; // Generate unique filename
+        $imagePath = $uploadDirectory . $imageName;
+        $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+        file_put_contents($imagePath, $imageData);
+        $imagePaths[] = $imagePath;
+    }
 
     // Include the file containing the function to add a blog
     include_once '../../model/seller/addBlog.php';
 
     // Call the function to add the blog post
-    $success = addBlog($mysqli, $content, $title);
+    $success = addBlog($mysqli, $content, $title, $imagePaths);
 
     // Check if the addition was successful
     if ($success) {
